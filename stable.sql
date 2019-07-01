4933825a6da959f43541d3b8443bb7e662c58468
hasiniishara <40210219+hasiniishara@users.noreply.github.com>
2019-07-01 17:36:50 +0530

    Add files via upload

---
+++ stable.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2019 at 11:00 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stable`
+--
+
+CREATE TABLE `stable` (
+  `Id` int(4) NOT NULL,
+  `staffname` varchar(20) NOT NULL,
+  `month` varchar(10) NOT NULL,
+  `s_perday` int(5) NOT NULL,
+  `attendance` int(3) NOT NULL,
+  `salary` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stable`
+--
+
+INSERT INTO `stable` (`Id`, `staffname`, `month`, `s_perday`, `attendance`, `salary`) VALUES
+(1, 'hasini', 'january', 200, 30, 6000),
+(2, 'anjana', 'january', 200, 21, 4200),
+(3, 'hansani', 'january', 200, 25, 5000),
+(4, 'sandya', 'january', 200, 25, 5000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stable`
+--
+ALTER TABLE `stable`
+  ADD PRIMARY KEY (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
